fe7546539bd024ed73fecd8f48bf2a572d5011a3
光天化日之下 <570929310@qq.com>
2025-06-15 20:37:41 +0800

    express-test-1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3006;
 
 app.get("/", (req, res) => {
-  res.send("Hello QSB");
+  res.send("<h1>Hello World!!!!!</h1>");
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
